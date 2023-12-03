a8dc8d16ef27f2f0c1c822b491a8ac3787085deb
Jiji14 <jijeong0114@gmail.com>
2023-12-02 18:03:44 -0800

    change file path

--- src/screens/MyDay.tsx
+++ src/screens/MyDay.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import Setting from '../coponents/Setting';
+import Setting from './Setting';
 
 export default function MyDay() {
   return (
