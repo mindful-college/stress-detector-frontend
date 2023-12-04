a9cab55b10c719c9dc89493e57193798f693e7f2
Jiji14 <jijeong0114@gmail.com>
2023-12-03 23:59:46 -0800

    SignIn page design fix and add input check

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import { Colors } from '../utils/colors';
 import { useUserContext } from '../context/UserContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SING_IN_URL } from '../utils/api';
 import axios from 'axios';
 import Toast from 'react-native-toast-message';
-import { emailRegex } from '../utils/common';
+import { emailRegex, passwordRegex } from '../utils/common';
+import CustomButton from '../coponents/CustomButton';
+import CustomLink from '../coponents/CustomLink';
 
-export default function Signin() {
+export default function Signin({ navigation }) {
   const [user, setUser] = useState({ email: '', password: '' });
   const { dispatch } = useUserContext();
 
@@ -16,7 +27,14 @@ export default function Signin() {
     if (!emailRegex.test(user.email)) {
       Toast.show({
         type: 'error',
-        text1: 'Invalid email format',
+        text1: 'Email must be valid format',
+      });
+      return;
+    }
+    if (!passwordRegex.test(user.password)) {
+      Toast.show({
+        type: 'error',
+        text1: 'Password must be at least 8 characters',
       });
       return;
     }
@@ -51,44 +69,105 @@ export default function Signin() {
     setUser((prevUser) => ({ ...prevUser, password }));
   };
 
+  const goToSignUp = () => {
+    navigation.navigate('SignUp');
+  };
+
+  const checkIfAllFieldsNotEmpty = () => {
+    return user.email && user.password;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Sign In</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={user.email}
-        onChangeText={handleEamil}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={user.password}
-        onChangeText={handlePassword}
-        secureTextEntry
-      />
-      <Button title="Sign In" onPress={handleSignIn} />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <View style={styles.logo}>
+          <Text style={styles.logoText}>STRESS</Text>
+          <Text style={styles.logoText}>DETECTOR</Text>
+        </View>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={styles.inputWrapper}>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={user.email}
+            onChangeText={handleEamil}
+            keyboardType="email-address"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            value={user.password}
+            onChangeText={handlePassword}
+            secureTextEntry
+          />
+          {checkIfAllFieldsNotEmpty() ? (
+            <CustomButton
+              color={Colors.white}
+              backgroundColor={Colors.primary}
+              title="Sign In"
+              onPress={handleSignIn}
+            />
+          ) : (
+            <CustomButton
+              color={Colors.black}
+              backgroundColor={Colors.grey}
+              title="Sign In"
+              disabled={true}
+            />
+          )}
+          <View style={styles.linkWrapper}>
+            <CustomLink handleNavigate={goToSignUp} color={Colors.primary} text="Sign Up" />
+            <CustomLink handleNavigate={goToSignUp} color={Colors.primary} text="Reset Password" />
+          </View>
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    padding: 16,
+    justifyContent: 'flex-end',
+    backgroundColor: Colors.primary,
+  },
+  inputWrapper: {
+    backgroundColor: Colors.white,
+    paddingHorizontal: 16,
+    paddingVertical: 40,
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 16,
+    marginBottom: 40,
   },
   input: {
-    height: 40,
+    height: 48,
     borderColor: Colors.grey,
     borderWidth: 1,
     marginBottom: 16,
     padding: 8,
+  },
+  linkWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 20,
+    marginVertical: 20,
+  },
+  logo: {
+    position: 'absolute',
+    top: 120,
+    display: 'flex',
+    alignItems: 'center',
+    gap: 4,
+    width: '100%',
+  },
+  logoText: {
+    fontSize: 36,
+    color: Colors.white,
+    fontWeight: '600',
   },
 });
