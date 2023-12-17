e3d4f98f77cbc7177db0f72ae77ff20adddea11f
Jiji14 <jijeong0114@gmail.com>
2023-12-16 17:42:47 -0800

    signout - user state update error fix

--- src/screens/Setting.tsx
+++ src/screens/Setting.tsx
@@ -1,21 +1,35 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import { Button, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { initialUser, userReducer } from '../context/UserContext';
+import { useUserContext } from '../context/UserContext';
 import { SING_OUT_URL } from '../utils/api';
 import axios from 'axios';
+import Toast from 'react-native-toast-message';
 
 export default function Setting() {
-  const [state, dispatch] = useReducer(userReducer, initialUser);
+  const { state, dispatch } = useUserContext();
 
   const handleSignOut = async () => {
-    const headers = {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${state.user?.access_token}`,
-    };
-    const res = await axios.post(SING_OUT_URL, { headers });
-    AsyncStorage.removeItem('user');
-    dispatch({ type: 'REMOVE_USER' });
+    try {
+      const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${state.user?.access_token}`,
+      };
+      const { status } = await axios.post(SING_OUT_URL, { headers });
+      if (status === 200) {
+        AsyncStorage.removeItem('user');
+        dispatch({ type: 'REMOVE_USER' });
+        Toast.show({
+          type: 'success',
+          text1: 'See you agian!',
+        });
+      }
+    } catch (e) {
+      Toast.show({
+        type: 'error',
+        text1: 'Network Error',
+      });
+    }
   };
 
   return (
