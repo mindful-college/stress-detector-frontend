ff0520e9f689e949a447a6cb469a417224e6921d
Jiji14 <jijeong0114@gmail.com>
2023-12-30 14:20:43 -0800

    apply prettier

--- src/types/checkin.ts
+++ src/types/checkin.ts
@@ -6,14 +6,25 @@ export type Conversation = {
   showIcon?: number;
 };
 
-export type ChatbotKey = 
-    'init' | 'greeting' | 'followup' | 'text' | 'voice' | 'study_hours' | 'studyError' | 
-    'work_hours' | 'workError' | 'confirm' | 'stress_level' | 'closing' | 'restart';
+export type ChatbotKey =
+  | 'init'
+  | 'greeting'
+  | 'followup'
+  | 'text'
+  | 'voice'
+  | 'study_hours'
+  | 'studyError'
+  | 'work_hours'
+  | 'workError'
+  | 'confirm'
+  | 'stress_level'
+  | 'closing'
+  | 'restart';
 
 export type Report = {
-    text: string[];
-    voice: string[];
-    study_hours: number | null;
-    work_hours: number | null;
-    stress_level: number | null;
-}
+  text: string[];
+  voice: string[];
+  study_hours: number | null;
+  work_hours: number | null;
+  stress_level: number | null;
+};
