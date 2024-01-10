1cce9acc29566cf3822abbe09455a4e7e4c54bb6
Jiji14 <jijeong0114@gmail.com>
2024-01-10 00:37:24 -0800

    update prod api address

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,7 @@
-const DEFAULT_URL = !process.env.NODE_ENV || process.env.NODE_ENV ? 'http://127.0.0.1:8000' : '';
+const DEFAULT_URL =
+  process.env?.NODE_ENV === 'development'
+    ? 'http://127.0.0.1:8000'
+    : 'https://mindful-college-2394382a42bf.herokuapp.com';
 export const TEST_URL = `${DEFAULT_URL}/`;
 export const SING_IN_URL = `${DEFAULT_URL}/v1/signin`;
 export const SING_UP_URL = `${DEFAULT_URL}/v1/signup`;
